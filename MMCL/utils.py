309adde289b99fc16ed54005d6e18451101574fc
Hadi Ghaemi <hadi.qaemi@gmail.com>
2024-02-02 10:41:16 +0200

    read config

--- MMCL/utils.py
+++ MMCL/utils.py
@@ -1,15 +1,23 @@
 from datasets import load_dataset
 
-def load_data(train_dataset, test_dataset, tages_files):
+import argparse
+import yaml
+
+def load_config_as_namespace(config_file):
+    with open(config_file, "r") as f:
+        config_dict = yaml.safe_load(f)
+    return argparse.Namespace(**config_dict)
+
+def load_data(config_file):
   dataset = load_dataset(
       "csv",
       data_files={
-          "train": train_dataset,
-          "test": test_dataset
+          "train": config_file.train_dataset,
+          "test": config_file.test_dataset
       }
   )
 
-  with open(tages_files) as f:
+  with open(config_file.tages_files) as f:
       tags_space = f.read().splitlines()
 
   return dataset.map(
